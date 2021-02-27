aa32d1f80b8a8be3f8ee3b4ab65300528e0315ad
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-02-27 22:59:41 +0100

    you want to f my code

--- src/clients/src/Client.java
+++ src/clients/src/Client.java
@@ -19,12 +19,11 @@ public class Client {
     private  void menu(){
         System.out.println("|***********************************MENU************************************|");
         System.out.println("|_____________________________Choose number_________________________________|");
-        System.out.println("| (1) print student                                                         |");
-        System.out.println("| (2) destroy a student                                                     |");
-        System.out.println("| (3) add a new student                                                     |");
-        System.out.println("|                                (0) Exit                                   |");
-        System.out.println("|______________________________Developer Options____________________________|");
-        System.out.println("| (7) Print Exceptions log                                                  |");
+        System.out.println("| (1)       print student                                                   |");
+        System.out.println("| (2)       add a student                                               |");
+        System.out.println("| (3)       destroy a new student                                               |");
+        System.out.println("|________________________________(0) Exit___________________________________|");
+
     }
     public void communicate(){
         while(!isConnected){
@@ -36,20 +35,56 @@ public class Client {
                 isConnected = false;
                 System.out.println("choose well :  ");
                 this.menu();
-                Boolean ok = true;
-                Scanner choice = new Scanner(System.in);
-                while(ok){
-                    switch(choice.nextInt()){
-                        case 0 :
-                            System.out.println("thank you for your service");
-                            ok = false;                        
+               
+                    switch(new Scanner(System.in).nextInt()){
+                        case 0 :{
+                            System.out.println("thank you for your service");                        
                             break;
+                        }
+                        case 1: {
+                            System.out.println("Insert the id to look for...");
+                            Scanner search = new Scanner(System.in);
+                            ObjectOutputStream objectoutstream = new ObjectOutputStream(socket.getOutputStream());
+                            objectoutstream.writeObject("print");
+                            objectoutstream.writeObject(search);
+                            System.out.println("your request has been sent");
+                            break;
+
+                        }
+                        case 3 : {
+                            System.out.println("Insert the id of your target...");
+                            Scanner delete = new Scanner(System.in);
+                            ObjectOutputStream objectoutstream = new ObjectOutputStream(socket.getOutputStream());
+                            objectoutstream.writeObject("delete");
+                            objectoutstream.writeObject(delete);
+                            System.out.println("your request has been sent");
+                            break;
+                        }
+                        case 2 : {
+                            System.out.println("Insert the id ...");
+                            Scanner add = new Scanner(System.in);
+                            ObjectOutputStream objectoutstream = new ObjectOutputStream(socket.getOutputStream());
+                            int mat = add.nextInt();
+                            System.out.println("what is the name ? ");
+                            String name = add.nextLine().strip();
+                            System.out.println("is your last name cool ? ");
+                            String lastname = add.nextLine().strip();
+                            Student puppet = new Student(mat, name, lastname);
+                            objectoutstream.writeObject("add");
+                            objectoutstream.writeObject(puppet);
+                            break;
+                        }
                     }
-                }
+                
             }catch(Exception e) {
                 ExceptionSaver es = new ExceptionSaver() ;
                 es.save(e);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Client guest = new Client();
+        guest.communicate();
     }
 }
--- src/clients/src/HiddenClient.java
+++ src/clients/src/HiddenClient.java
@@ -1,5 +1,10 @@
 package clients.src;
-
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class HiddenClient {
     
 }
