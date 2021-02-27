37f9953df2e28a464e7de1096d8df3b3ad167024
thbob2 <33073278+thbob2@users.noreply.github.com>
2021-02-27 19:59:11 +0100

    in the begging we may think we know why it works

--- src/clients/src/Client.java
+++ src/clients/src/Client.java
@@ -1,5 +1,11 @@
 package clients.src;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
 
+/** 
+ * ! P1 
+ * */
 public class Client {
     
 }
---
+++ src/clients/src/Student.java
@@ -0,0 +1,79 @@
+package clients.src;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    
+    private int mat;
+    private String name;
+    private String lastName;
+    
+
+    /**
+     * @return int return the mat
+     */
+    public int getMat() {
+        return mat;
+    }
+
+    /**
+     * @param mat the mat to set
+     */
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Student(int mat,String name,String lastName){
+        this.mat = mat;
+        this.name = name;
+        this.lastName = lastName;
+    }
+    public String toString(){
+        return mat + "| "+name+"| "+lastName;
+
+    }
+
+    public int hashCode(){
+        return mat;
+    }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Student student = (Student) o;
+        
+        if (this.mat != student.getMat()) return false;
+        if (name != null ? !name.equals(student.getName()) : student.getName() != null) return false;
+        
+        return true;
+        }
+}
+
